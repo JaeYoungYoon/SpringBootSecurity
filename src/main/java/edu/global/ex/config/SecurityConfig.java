92954237979a5353abbcf799f97fe1bf517ef34d
Jane <jy345678@naver.com>
2023-04-17 15:43:28 +0900

    SecurityConfig

---
+++ src/main/java/edu/global/ex/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package edu.global.ex.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable(); // CSRF 설정 해제 (초기 개발시 꺼놓고 작업)
+
+		http.authorizeRequests()
+				// .antMatchers("/user/**").hasAnyRole("USER")
+				// .antMatchers("/admin/**").hasAnyRole("ADMIN")
+				.antMatchers("/**").permitAll();
+
+		http.formLogin();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER").and().withUser("admin")
+				.password("{noop}admin").roles("ADMIN");
+
+	}
+
+}
