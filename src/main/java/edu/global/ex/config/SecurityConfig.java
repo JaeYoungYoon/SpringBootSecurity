d2c65b8271d735b897c9e1ed0fb2cec9f926840f
Jane <jy345678@naver.com>
2023-04-18 12:34:52 +0900

    유저 정보 관리

--- src/main/java/edu/global/ex/config/SecurityConfig.java
+++ src/main/java/edu/global/ex/config/SecurityConfig.java
@@ -1,22 +1,27 @@
 package edu.global.ex.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import edu.global.ex.security.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable(); // CSRF 설정 해제 (초기 개발시 꺼놓고 작업)
+		http.csrf().disable();
 
-		http.authorizeRequests()
-				.antMatchers("/user/**").hasAnyRole("USER")
-				.antMatchers("/admin/**").hasAnyRole("ADMIN")
+		http.authorizeRequests().antMatchers("/user/**").hasAnyRole("USER").antMatchers("/admin/**").hasAnyRole("ADMIN")
 				.antMatchers("/**").permitAll();
 
 		http.formLogin();
@@ -24,10 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-		auth.inMemoryAuthentication().withUser("user").password("{noop}user").roles("USER").and().withUser("admin")
-				.password("{noop}admin").roles("ADMIN");
-
+		auth.userDetailsService(customUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
 	}
 
 }
---
+++ src/main/java/edu/global/ex/vo/UserDetailsVO.java
@@ -0,0 +1,87 @@
+package edu.global.ex.vo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsVO implements UserDetails {
+
+	private String username;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserDetailsVO(UserVO user) {
+		this.setAuthorities(user);
+		this.setPassword(user.getPassword());
+		this.setUsername(user.getUsername());
+	}
+
+	// setter
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	// setter
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// setter
+	public void setAuthorities(UserVO userVO) {
+
+		List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+
+		for (AuthVO auth : userVO.getAuthList()) {
+			authorities.add(new SimpleGrantedAuthority(auth.getAuthority()));
+		}
+
+		this.authorities = authorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return username;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
