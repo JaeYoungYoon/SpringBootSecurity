19dd55da747d8caf35e805c2882207e80b1a2f5a
Jane <jy345678@naver.com>
2023-04-21 12:04:27 +0900

    암호화 모듈 설명

--- src/main/java/edu/global/ex/config/SecurityConfig.java
+++ src/main/java/edu/global/ex/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package edu.global.ex.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.global.ex.security.CustomUserDetailsService;
+import edu.global.ex.vo.UserVO;
 
 @Configuration
 @EnableWebSecurity
@@ -17,6 +20,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
+	@Bean
+	public PasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public UserVO userVO() {
+		UserVO vo = new UserVO();
+		vo.setPassword("메롱");
+		vo.setUsername("메롱");
+		return vo;
+	}
+	
+	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
@@ -24,8 +42,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/user/**").hasAnyRole("USER").antMatchers("/admin/**").hasAnyRole("ADMIN")
 				.antMatchers("/**").permitAll();
 
-		//http.formLogin();
-		
+		// http.formLogin();
+
 		http.formLogin().loginPage("/login").usernameParameter("id").passwordParameter("pw").permitAll();
 	}
 
--- src/test/java/edu/global/ex/mapper/UserMapperTest.java
+++ src/test/java/edu/global/ex/mapper/UserMapperTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.global.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
@@ -16,34 +17,44 @@ class UserMapperTest {
 
 	@Autowired
 	private UserMapper userMapper;
-
-	@Test
-	void testInsertUser() {
-
-//      @Insert("insert into users(username,password,enabled) values(#{username},#{password},#{enabled})")
-//      public int insertUser(UserVO userVO);
-//
-//      @Insert("insert into AUTHORITIES (username,AUTHORITY) values(#{username},'ROLE_USER')")
-//      public void insertAuthorities(UserVO UserVO);
-
-		UserVO user = new UserVO();
-		user.setUsername("kim4");
-		user.setPassword(new BCryptPasswordEncoder().encode("1234"));
-		user.setEnabled(1);
-
-		userMapper.insertUser(user);
-		userMapper.insertAuthorities(user);
-	}
 	
-	@Test
-	void testInsertAdminUser() {
-		UserVO user = new UserVO();
-		user.setUsername("admin2");
-		user.setPassword(new BCryptPasswordEncoder().encode("admin2"));
-		user.setEnabled(1);
+	@Autowired
+	private UserVO userVO;
 
-		userMapper.insertUser(user);
-		userMapper.insertAdminAuthorities(user);
+	/*
+	 * @Test void testInsertUser() {
+	 * 
+	 * UserVO user = new UserVO(); user.setUsername("kim4"); user.setPassword(new
+	 * BCryptPasswordEncoder().encode("1234")); user.setEnabled(1);
+	 * 
+	 * userMapper.insertUser(user); userMapper.insertAuthorities(user); }
+	 * 
+	 * @Test void testInsertAdminUser() { UserVO user = new UserVO();
+	 * user.setUsername("admin2"); user.setPassword(new
+	 * BCryptPasswordEncoder().encode("admin2")); user.setEnabled(1);
+	 * 
+	 * userMapper.insertUser(user); userMapper.insertAdminAuthorities(user); }
+	 */
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Test
+	void testPasswordEncoder() {
+		String plainPW = "12345";
+		String encodedPW = passwordEncoder.encode(plainPW);
+
+		System.out.println("plain : " + plainPW + " / encoded : " + encodedPW);
+		System.out.println(passwordEncoder.matches(plainPW, encodedPW));
+		
+		
+		System.out.println(userVO);
+
+		/* ============================================================== */
+
+		// assertNotEquals(plainPW, encodedPW);
+		// assertEquals(plainPW, encodedPW);
+		// assertTrue(new BCryptPasswordEncoder().matches(plainPW, encodedPW));
 	}
 
 }
